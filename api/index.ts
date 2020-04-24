584ea5b6092c6786920771f0e5252e3dac28cb44
George Karagkiaouris <gkaragkiaouris2@gmail.com>
2020-04-24 19:56:41 -0400

    Initial commit

---
+++ api/index.ts
@@ -0,0 +1,19 @@
+import {NowRequest, NowResponse} from '@now/node'
+import got from 'got';
+
+export default async (req: NowRequest, res: NowResponse) => {
+  const {body} = await got.get<any>('https://api.github.com/repos/wulkano/kap/releases/latest', {
+    responseType: 'json'
+  });
+
+  const dmgArtifact = (body.assets || []).find(asset => asset.name.includes('dmg'));
+  const dmgLink = dmgArtifact && dmgArtifact.browser_download_url;
+
+  if (dmgLink) {
+    res.writeHead(302, {
+      Location: dmgLink
+    });
+  } else {
+    res.status(404);
+  }
+}
